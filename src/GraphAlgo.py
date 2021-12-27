1cd2d26fc68a561d90b0e630cfd4dae7a24f37fc
Rashipachino <rashipachino@gmail.com>
2021-12-27 23:21:55 +0200

    notes and comments

--- src/GraphAlgo.py
+++ src/GraphAlgo.py
@@ -68,42 +68,46 @@ class GraphAlgo(GraphAlgoInterface, ABC):
         @return: The distance of the path, a list of the nodes ids that the path goes through
         """
         global node_weight
-        if id1 not in self.graph.nodes or id2 not in self.graph.nodes: # If there is no path between id1 and id2, or one of them dose not exist the function returns (float('inf'),[])
+        if id1 not in self.graph.nodes or id2 not in self.graph.nodes:  # If there is no path between id1 and id2, or one of them dose not exist the function returns (float('inf'),[])
             return float('inf'), []
-        if id1 == id2: # if both ids entered are the same, the path is just the node itself and the cost is zero
+        if id1 == id2:  # if both ids entered are the same, the path is just the node itself and the cost is zero
             return 0.0, [id1]
-        dijakstra = {i: float('inf') for i in self.graph.nodes} # turning all "weights" to infinity
-        dijakstra[id1] = 0 # turns start node's "weight" to 0
+        dijakstra = {i: float('inf') for i in self.graph.nodes}  # turning all "weights" to infinity
+        dijakstra[id1] = 0  # turns start node's "weight" to 0
         pq = []
-        heapq.heapify(pq)
         prev = {}
-        for n in self.graph.nodes:
+        for n in self.graph.nodes:  # assign all nodes prev = None
             prev[n] = None
             if n != id1:
-                heapq.heappush(pq, (float('inf'), n))
+                heapq.heappush(pq, (float('inf'), n))  # push into heapified list pq
             else:
                 heapq.heappush(pq, (0, n))
         while len(pq) != 0:
             node_weight, i = heapq.heappop(pq)
-            if dijakstra[i] == float('inf'):
+            if dijakstra[i] == float('inf'):  # if popping a node who's weight is infinity, no path exists
                 return float('inf'), []
-            if i == id2:
+            if i == id2:  # found path
                 break
-            for dest, w in self.graph.all_out_edges_of_node(i).items():
+            for dest, w in self.graph.all_out_edges_of_node(i).items():  # all neighbors of i
                 if dijakstra[dest] > dijakstra[i] + w:
-                    pq.remove((dijakstra[dest], dest))
-                    dijakstra[dest] = dijakstra[i] + w
-                    prev[dest] = i
-                    heapq.heappush(pq, (dijakstra[dest], dest))
+                    pq.remove((dijakstra[dest], dest))  # removes id from pq
+                    dijakstra[dest] = dijakstra[i] + w  # relaxes
+                    prev[dest] = i  # updates prev
+                    heapq.heappush(pq, (dijakstra[dest], dest)) # push id back into pq with new weight
         temp = id2
         short_path = [temp]
-        while prev[temp] is not None:
+        while prev[temp] is not None:  # create list path
             short_path.insert(0, prev[temp])
             temp = prev[temp]
         return node_weight, short_path
 
     def TSP(self, node_lst: List[int]) -> (List[int], float):
-        if len(node_lst) == 1:
+        """
+        Finds the shortest path that visits all the nodes in the list. Uses helper function closestNodeFinder to
+        locate which node in the list is closest to the current node :param node_lst: A list of nodes id's :return: A
+        list of the nodes id's in the path, and the overall distance
+        """
+        if len(node_lst) == 1: # one value in list, return just itself and cost is zero
             return node_lst[0], 0.0
         global path, cost
         min_weight = float('inf')
@@ -114,48 +118,56 @@ class GraphAlgo(GraphAlgoInterface, ABC):
             cost = 0.0
             curr = city
             while True:
-                cities.remove(curr)
-                if len(cities) == 1:
+                cities.remove(curr)  # remove curr from cities before sending to helper function in order to actually
+                # find closest node not including the node itself
+                if len(cities) == 1:  # only one left
                     break
-                closest_node_path = self._closestNodeFinder(cities, curr)
+                closest_node_path = self._closestNodeFinder(cities, curr)  # returns the shorest path to the closest
+                # node and the price is cost to get there
                 if closest_node_path == -1:
                     return [], -1
-                path += closest_node_path[0][1:]
-                cost += closest_node_path[1]
-                curr = closest_node_path[0][-1]
-            closest_last = self.shortest_path(curr, cities[0])
-            path += closest_last[1][1:]
-            cost += closest_last[0]
-            if cost < min_weight:
+                path += closest_node_path[0][1:]  # add path except for first value because already added to path
+                cost += closest_node_path[1]  # update cost
+                curr = closest_node_path[0][-1] # curr now equals what was the next closest node
+            closest_last = self.shortest_path(curr, cities[0])  # finds shortest path from last closest node to last
+            # value in cities
+            path += closest_last[1][1:]  # adds to path
+            cost += closest_last[0]  # updates cost
+            if cost < min_weight:  # if this path is really the least costly path, make the path and cost the return values
                 min_path = path.copy()
                 min_weight = cost
         return min_path, min_weight
 
     def _closestNodeFinder(self, n_list: List[int], curr_node: int) -> (List[int], float):
+        """
+        TSP helper function. finds the closest node in the given list to the curr_node
+        @param n_list: List of cities to visit
+        @param curr_node: starter node
+        @return: returns the shortest path from curr_node to closest node in list and the cost to get there
+        """
         global curr, node_w
         path_w = 0.0
-        dijkstra = {i: float('inf') for i in self.graph.nodes}
-        dijkstra[curr_node] = 0
+        dijkstra = {i: float('inf') for i in self.graph.nodes}  # turning all "weights" to infinity
+        dijkstra[curr_node] = 0  # turns "weight" of starter node 0
         pq = []
-        heapq.heapify(pq)
         prev = {}
         for n in self.graph.nodes:
             prev[n] = None
             if n != curr_node:
-                heapq.heappush(pq, (float('inf'), n))
+                heapq.heappush(pq, (float('inf'), n)) # push into heapified list pq
             else:
-                heapq.heappush(pq, (0, n))
+                heapq.heappush(pq, (0, n))  # push into heapified list pq
         while len(pq) != 0:
-            node_w, curr = heapq.heappop(pq)
-            if curr in n_list:
+            node_w, curr = heapq.heappop(pq)  # pops lowest weighted value in pq
+            if curr in n_list:  #found value in list
                 break
-            for neighbor, w in self.graph.all_out_edges_of_node(curr).items():
+            for neighbor, w in self.graph.all_out_edges_of_node(curr).items():  # check neighbors
                 if dijkstra[neighbor] > dijkstra[curr] + w:
-                    pq.remove((dijkstra[neighbor], neighbor))
-                    dijkstra[neighbor] = dijkstra[curr] + w
-                    prev[neighbor] = curr
-                    heapq.heappush(pq, (dijkstra[neighbor], neighbor))
-        if len(pq) == 0:
+                    pq.remove((dijkstra[neighbor], neighbor))  # removes id from pq
+                    dijkstra[neighbor] = dijkstra[curr] + w  #relaxes
+                    prev[neighbor] = curr  # updates prev
+                    heapq.heappush(pq, (dijkstra[neighbor], neighbor))  # push id back into pq with new weight
+        if len(pq) == 0:  # got to end of list and did not find a closest node
             return -1
         temp_node = curr
         path_w += node_w
@@ -166,6 +178,11 @@ class GraphAlgo(GraphAlgoInterface, ABC):
         return dij_path, node_w
 
     def centerPoint(self) -> (int, float):
+        """
+        Finds the node that has the shortest distance to it's farthest node. This function uses a helper function
+        eccentricity.
+        @return: The nodes id, min-maximum distance
+        """
         global n
         min_weight = float('inf')
         center_node = -1
@@ -202,4 +219,7 @@ class GraphAlgo(GraphAlgoInterface, ABC):
         return -1, float('inf')
 
     def plot_graph(self) -> None:
+        """
+        We used a GUI to represent the graph and perform actions on it
+        """
         WindowGUI.game(self)
